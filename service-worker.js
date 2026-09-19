b851b6cd14912c93382b45fc367dbd41ca96c61e
ucnwtf1-wq <ucn.wtf1@gmail.com>
2026-09-19 14:05:17 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,7 @@
 // prochain lancement de l'application.
 // ============================================================
 
-const VERSION = 'v1';
+const VERSION = 'v2';
 const CACHE_NAME = 'brocante-export-' + VERSION;
 
 const FICHIERS_A_METTRE_EN_CACHE = [
