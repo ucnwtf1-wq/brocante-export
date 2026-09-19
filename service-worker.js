baf5ba845f924e0067b627fafae44012c3d33ae3
ucnwtf1-wq <ucn.wtf1@gmail.com>
2026-09-19 14:46:13 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,7 @@
 // prochain lancement de l'application.
 // ============================================================
 
-const VERSION = 'v3';
+const VERSION = 'v4';
 const CACHE_NAME = 'brocante-export-' + VERSION;
 
 const FICHIERS_A_METTRE_EN_CACHE = [
