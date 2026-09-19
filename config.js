d42f3832a9e653c2b615e93fa33f4ac022d61263
ucnwtf1-wq <ucn.wtf1@gmail.com>
2026-09-19 13:50:28 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // ============================================================
 // Coller ici l'URL du Web App Google Apps Script (celle qui se
 // termine par /exec), obtenue après "Déployer > Nouveau déploiement".
-window.BACKEND_URL = "https://script.google.com/macros/s/AKfycbyn9g_YfPbOSVDHgdrfHtQNn-mCIFBeXzrACCwbR9-ARiiLva5UvBPlOza0JtgwB3wLBA/exec";
+window.BACKEND_URL = "https://script.google.com/macros/s/AKfycbzP1S5tOA14sLnBkjsrlwifXUXBKVrjlvdbAJQRO4GNMsRglGJeMIJeV3rTlJIKQmK8qQ/exec";
